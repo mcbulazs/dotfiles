9a3f83a318c32186ec421530666e5a167d60d042
Vari Balazs <vari.balazs01@gmail.com>
2024-10-17 15:24:23 +0200

    testing

--- package-installer.sh
+++ package-installer.sh
@@ -1,16 +1,36 @@
 !#/usr/bin/bash
 
-git clone https://aur.archlinux.org/yay-bin.git "$HOME/yay-bin"
-cd "$HOME/yay-bin"
-makepkg -si
 
-sudo pacman -S --needed git zip unzip make fzf stow fd less man-db iwd pulseaudio brightnessctl ripgrep vim wget xclip xdg-utils xorg xorg-xinit tree make base-devel
+confirmation() {
+    read -p "$1 (Y/N): " confirm
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+        return 0
+    fi
+    return 1
+}
 
-sudo pacman -S --needed pulseaudio pulseaudio-bluetooth blueberry
+confirmation "This will take several minutes. Are you sure you want to continue?" || exit 1
 
-sudo pacman -S --needed bspwm sxhkd polybar kitty dunst pavucontrol fastfetch rofi rofi-calc rofi-emoji yazi neovim picom feh flameshot starship ttf-jetbrains-mono-nerd hyprland waybar hyprlock hypridle xwaylandvideobridge xorg-xwayland hyprpaper hyprshot xdg-desktop-portal-hyprland
+#yay installation
+if ! which yay &> /dev/null; then
+    if confirmation "Do you want to install yay (unofficial package manger)?"; then
+        sudo pacman -S --needed git base-devel
+        git clone https://aur.archlinux.org/yay-bin.git "$HOME/yay-bin"
+        cd "$HOME/yay-bin"
+        makepkg -si
+    fi
+fi
 
-yay -S rofi-greenclip
+confirmation "Do you want to install must-have packages?" &&
+  sudo pacman -S --needed git zip unzip make fzf stow fd less man-db iwd pulseaudio brightnessctl ripgrep vim wget xclip xdg-utils xorg xorg-xinit tree make base-devel
 
-sudo pacman -S zsh
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+confirmation "Do you want to install BLUETOOTH specific packages?" &&
+    sudo pacman -S --needed pulseaudio pulseaudio-bluetooth blueberry
+confirmation "Do you want to install personal config packages?" && {
+  sudo pacman -S --needed bspwm sxhkd polybar kitty dunst pavucontrol fastfetch rofi rofi-calc rofi-emoji yazi neovim picom feh flameshot starship ttf-jetbrains-mono-nerd hyprland waybar hyprlock hypridle xwaylandvideobridge xorg-xwayland hyprpaper hyprshot xdg-desktop-portal-hyprland
+  yay -S rofi-greenclip hyprshot
+}
+confirmation "Do you want to install zsh?" && {
+    sudo pacman -S zsh
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+}
